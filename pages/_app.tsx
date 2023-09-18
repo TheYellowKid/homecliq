25ac1427eff7589c60b265157131c349b53068fd
codesleyah <developer.kax@gmail.com>
2023-09-18 16:10:58 +0200

    added clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="bg-gray-100 font-quicksand">
+      <Head>
+        <Script
+          id="ms_clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "ix21hmdlo9");`,
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
