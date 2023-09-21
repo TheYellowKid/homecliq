f4092363b36d28407052ba1cc51baf6dd1356575
codesleyah <developer.kax@gmail.com>
2023-09-21 14:20:30 +0200

    new clarity config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,11 +12,11 @@ function MyApp({ Component, pageProps }: AppProps) {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "ix21hmdlo9");`,
+           (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "ix21hmdlo9");`,
           }}
         />
       </Head>
